b52fe0ead70087204abdf38a9ef2dbbb26b4f4be
Mat Werber <matwerber@gmail.com>
2020-05-20 12:43:20 -0700

    Updates

---
+++ precache-manifest.134901023c3784d444f615c086ca12a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acbb83c9549a48cf00d72522828af585",
+    "url": "/aws-tester-project/index.html"
+  },
+  {
+    "revision": "21b82e396a2c9bf1021f",
+    "url": "/aws-tester-project/static/css/2.dbecbe62.chunk.css"
+  },
+  {
+    "revision": "487dd3af1e57194ae489",
+    "url": "/aws-tester-project/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "21b82e396a2c9bf1021f",
+    "url": "/aws-tester-project/static/js/2.36bad37e.chunk.js"
+  },
+  {
+    "revision": "b0bac01885cb262b4e7af1c25ff6d868",
+    "url": "/aws-tester-project/static/js/2.36bad37e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "487dd3af1e57194ae489",
+    "url": "/aws-tester-project/static/js/main.41e9b38a.chunk.js"
+  },
+  {
+    "revision": "f26282af0f73b89156ce",
+    "url": "/aws-tester-project/static/js/runtime-main.02bff158.js"
+  }
+]);
